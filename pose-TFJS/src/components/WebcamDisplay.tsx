d59d6c12b67844b7b35c155d7d26454495799af4
Janya Budaraju <budaraju.janya@gmail.com>
2024-06-12 11:26:29 -0400

    updating webcam display

--- pose-TFJS/src/components/WebcamDisplay.tsx
+++ pose-TFJS/src/components/WebcamDisplay.tsx
@@ -1,12 +1,17 @@
-import React, { useRef } from "react";
+import { useRef, useEffect, useCallback } from "react";
 import Webcam from "react-webcam";
+import * as posenet from "@tensorflow-models/posenet";
+import { drawKeypoints } from "../utils/utilities";
 
 // TODO: add prop for model
+
 function WebcamDisplay(){
     const camRef = useRef<Webcam>(null);
     const canvasRef = useRef<HTMLCanvasElement>(null);
+    const requestRef =  useRef<number | undefined>(undefined);
 
-    const runInference = async (net) => {
+    // function to run inference on a video frame and draw the corresponding keypoints.
+    const runInference = useCallback(async (net: posenet) => {
         if (camRef.current?.video?.readyState === 4) {
           const vid = camRef.current.video;
           const canvas = canvasRef.current;
@@ -19,18 +24,41 @@ function WebcamDisplay(){
             canvas.width = vidW;
             canvas.height = vidH;
 
-            const pred = await net.detect(vid);
-            // Draw mesh
-            const ctx = canvasRef.current.getContext("2d");
-            drawPred(pred, ctx);  // TODO: implement
+            const pose = await net.estimateSinglePose(vid);
+            console.log(pose);
+
+            const ctx = canvas.getContext("2d");
+            if(ctx) {
+              drawKeypoints(pose["keypoints"], 0.6, ctx);
+            }
           }
         }
-      };
+        requestRef.current = requestAnimationFrame(() => runInference(net));
+      }, []);
 
-    // const runModel = async () => {
-    //     // TODO: load model into net
+      // function to loop the runInference function for each frame of the video.
+      const runPosenet = useCallback(async () => {
+        const net = await posenet.load();
+        console.log("posenet model loaded.");
+        requestRef.current = requestAnimationFrame(() => runInference(net));
+      }, [runInference]);
 
-    // };
+      // hook to start the loop and clean up as necessary.
+      useEffect(() => {
+        runPosenet();
+        return () => {
+          if (requestRef.current) {
+            cancelAnimationFrame(requestRef.current);
+          }
+        };
+      }, [runPosenet]);
+
+      return (
+        <div>
+          <video ref={camRef} style={{ display: 'none' }} autoPlay playsInline muted></video>
+          <canvas ref={canvasRef}></canvas>
+        </div>
+      );
 }
 
 export default WebcamDisplay;
