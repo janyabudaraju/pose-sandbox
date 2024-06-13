7389eb5741d56a8425e7b1e932ff1268c35abe5f
Janya Budaraju <budaraju.janya@gmail.com>
2024-06-13 14:31:28 -0400

    adding fps

--- pose-TFJS/src/App.tsx
+++ pose-TFJS/src/App.tsx
@@ -5,13 +5,13 @@ import { Box, Flex, Heading } from "@chakra-ui/react";
 
 function App() {
     return (
-        <Box textAlign="center" p="4"> {/* Container for overall alignment and padding */}
-        <Heading mb="1">pose sandbox</Heading> {/* Webpage title */}
+        <Box textAlign="center" p="4">
+        <Heading mb="0">pose sandbox</Heading>
         <Flex direction="row" align="center" justify="space-between" w="100%" p="4">
-                <Box flex="1" minW="0" w="70%" overflow='auto'> {/* Ensures webcam display can shrink but fills space */}
-                <WebcamDisplay />
+                <Box flex="1" minW="0" w="70%" overflow='auto'>
+                    <WebcamDisplay />
                 </Box>
-                <Box flex="none" minW="200px" borderRadius="lg" p="10px"  m="2" borderWidth="3px" height='95vh'> {/* Control panel with fixed minimum width */}
+                <Box flex="none" minW="200px" borderRadius="lg" p="10px"  m="2" borderWidth="3px" height='95vh'>
                     <ModelSelect />
                 </Box>
             </Flex>
--- pose-TFJS/src/components/WebcamDisplay.tsx
+++ pose-TFJS/src/components/WebcamDisplay.tsx
@@ -1,14 +1,20 @@
-import { useRef, useEffect, useCallback } from "react";
+import { useRef, useEffect, useCallback, useState } from "react";
 import Webcam from "react-webcam";
 import * as posenet from "@tensorflow-models/posenet";
 import { drawKeypoints } from "../utils/utilities";
+import { Box } from "@chakra-ui/react";
 
 // TODO: add prop for model
 
 function WebcamDisplay(){
+
     const camRef = useRef<Webcam>(null);
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const requestRef =  useRef<number | undefined>(undefined);
+
+    const [fps, setFps] = useState(0);
+    const frameCount = useRef(0);
+    const prevTime = useRef(Date.now());
 
     // function to run inference on a video frame and draw the corresponding keypoints.
     const runInference = useCallback(async (net: posenet.PoseNet) => {
@@ -26,7 +32,7 @@ function WebcamDisplay(){
               canvas.width = vidW;
               canvas.height = vidH;
             }
-
+            // get prediction
             const pose = await net.estimateSinglePose(vid);
 
             const ctx = canvas.getContext("2d");
@@ -36,6 +42,7 @@ function WebcamDisplay(){
             }
           }
         }
+        frameCount.current += 1;
         requestRef.current = requestAnimationFrame(() => runInference(net));
       }, []);
 
@@ -56,59 +63,46 @@ function WebcamDisplay(){
         };
       }, [runPosenet]);
 
+      useEffect(() => {
+        const calculateFps = () => {
+          const now = Date.now();
+          const deltaTime = (now - prevTime.current) / 1000;
+          const resultFps = frameCount.current / deltaTime;
+          setFps(resultFps);
+          frameCount.current = 0;
+          prevTime.current = now;
+        };
+        // recalculate fps every second
+        const interval = setInterval(calculateFps, 1000);
+        return () => clearInterval(interval);
+      }, [fps]);
+
       return (
-        <div style={{ position: 'relative', width: '1280px', height: '720px' }} className="mirrored-container">
-          {/* <Webcam
-          ref={camRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            width: 640,
-            height: 480,
-          }}
-        />
-
-        <canvas
-          ref={canvasRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            width: 640,
-            height: 480,
-          }}
-        /> */}
-
-          <Webcam 
-            ref={camRef} 
-            style={{ 
-              position: 'absolute',
-              width: '100%',
-              height: '100%',
-              // top: 0,
-              // left: 0,
-            }} 
-            videoConstraints={{ width: 1280, height: 720, facingMode: "user" }}
-          />
-          <canvas 
-            ref={canvasRef} 
-            style={{ 
-              position: 'absolute',
-              width: '100%',
-              height: '100%',
-              // top: 0,
-              // left: 0,
-              zIndex: 10,
-              border: '4px solid black'
-            }}
-          />
+        <div style={{ position: 'relative'}}>
+          <div style={{ position: 'relative', width: '1280px', height: '720px' }} className="mirrored-container">
+            <Webcam 
+              ref={camRef} 
+              style={{ 
+                position: 'absolute',
+                width: '100%',
+                height: '100%',
+              }} 
+              videoConstraints={{ width: 1280, height: 720, facingMode: "user" }}
+            />
+            <canvas 
+              ref={canvasRef} 
+              style={{ 
+                position: 'absolute',
+                width: '100%',
+                height: '100%',
+                zIndex: 10,
+                border: '4px solid white'
+              }}
+            />
+          </div>
+          <Box position="absolute" top="0" left="0" p="2" bgColor="rgba(0,0,0,0.5)" color="white" fontSize="sm">
+            {fps.toFixed(1)} FPS
+          </Box>
         </div>
       );
 }
