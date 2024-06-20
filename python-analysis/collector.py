57cb667c1dd49a4d822b7577f37066b3e3dffd97
Janya Budaraju <budaraju.janya@gmail.com>
2024-06-20 10:51:08 -0400

    getting things to run (successful:P)

--- python-analysis/collector.py
+++ python-analysis/collector.py
@@ -1,24 +1,51 @@
-import cv2
+import cv2 as cv
 import numpy as np
 import tensorflow as tf
-import tensorflow_hub as hub
 import mediapipe as mp
-from mediapipe.tasks import vision
+from mediapipe.tasks.python import vision
 from mediapipe.tasks.python.vision import PoseLandmarker, PoseLandmarkerOptions
-from mediapipe.tasks.python.core import BaseOptions
+from mediapipe.tasks.python.core.base_options import BaseOptions
 
 def load_posenet():
-    model = hub.load("https://tfhub.dev/google/posenet/1")
-    return model
+    interpreter = tf.lite.Interpreter(model_path="python-analysis/models/posenet.tflite")
+    interpreter.allocate_tensors()
+    return interpreter
 
 def load_movenet():
-    model = hub.load("https://tfhub.dev/google/movenet/singlepose/lightning/4")
-    return model
+    interpreter = tf.lite.Interpreter(model_path='python-analysis/models/movenet-lightning.tflite')
+    interpreter.allocate_tensors()
+    return interpreter
 
-def load_blazepose(model_path = 'python-analysis/models/pose_landmarker_full.task'):
+def load_blazepose(model_path = 'python-analysis/models/blazepose.task'):
     options = PoseLandmarkerOptions(
         base_options=BaseOptions(model_asset_path=model_path),
         running_mode=vision.RunningMode.IMAGE)
-    landmarker = PoseLandmarker.create_from_options(options)
-    return landmarker
+    model = PoseLandmarker.create_from_options(options)
+    return model
 
+def run_inference_interpreter(interpreter, img, dtype=tf.uint8, shape=257):
+    input_image = cv.resize(img, (shape, shape))
+    input_image = tf.cast(input_image, dtype=dtype)
+    input_image = tf.expand_dims(input_image, axis=0)
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    interpreter.set_tensor(input_details[0]['index'], input_image.numpy())
+    interpreter.invoke()
+    kps = interpreter.get_tensor(output_details[0]['index'])
+    # TODO: convert to standard form
+    return kps
+
+def run_inference_mp(model, img):
+    mp_img = mp.Image(image_format=mp.ImageFormat.SRGB, data=img)
+    output = model.detect(mp_img)
+    # TODO: convert to standard form
+    return output
+
+POSENET_SHAPE = 257
+MOVENET_SHAPE = 192
+POSENET_DTYPE = tf.float32
+MOVENET_DTYPE = tf.uint8
+
+model = load_posenet()
+im = cv.imread('python-analysis/data/raw/test.jpg', cv.IMREAD_COLOR)
+op = run_inference_interpreter(model, im, dtype=tf.float32)
