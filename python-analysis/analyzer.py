1d37e750e816cf104b642566609b693724897417
Janya Budaraju <budaraju.janya@gmail.com>
2024-07-16 13:55:18 -0400

    refactoring

--- python-analysis/analyzer.py
+++ python-analysis/analyzer.py
@@ -1,21 +1,9 @@
 from definitions import Pose, Pose3D, KP2D, KP3D
-import definitions as defs
 import json
 import cv2 as cv
 import subprocess
 from pathlib import Path
-import subprocess
-
-def convert_webm_to_mp4(webm_path, mp4_path):
-    command = [
-        'ffmpeg',
-        '-i', webm_path,
-        '-c:v', 'libx264',
-        '-preset', 'fast',
-        '-c:a', 'aac',
-        mp4_path
-    ]
-    subprocess.run(command, check=True)
+import video_utils as vidutils
 
 def clean_dict_from_JSON(filepath: str):
     with open(filepath, 'r') as f:
@@ -26,7 +14,6 @@ def clean_dict_from_JSON(filepath: str):
     max_fn = -1
     prior_fn = -1
     max_ts = -1
-    min_ts = -1
 
     for entry in parsed:
         for pred in entry:
@@ -45,8 +32,6 @@ def clean_dict_from_JSON(filepath: str):
                 max_fn = frame_num
             if timestamp > max_ts:
                 max_ts = timestamp
-            if timestamp < min_ts:
-                min_ts = timestamp
 
             prior_fn = frame_num
 
@@ -80,35 +65,8 @@ def clean_dict_from_JSON(filepath: str):
     # print(json.dumps(parsed, indent=4))
     # with open('output.json', 'w') as f:
     #     json.dump(parsed, f, indent=4)
-    return formatted, max_fn, max_ts, min_ts
+    return formatted, max_fn, max_ts
 
-def get_timestamp_from_frame(vidpath):
-    cap = cv.VideoCapture(vidpath)
-
-    fps = cap.get(cv.CAP_PROP_FPS)
-    frame_count = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
-    duration = frame_count / fps
-    
-    print(f"total duration: {duration}s")
-    print(f"FPS: {fps}")
-    print(f"total number of frames: {frame_count}")
-
-    frame_no = 0
-    while(cap.isOpened()):
-        frame_exists, _ = cap.read()
-        if frame_exists:
-            print("for frame : " + str(frame_no) + "   timestamp is: ", str(cap.get(cv.CAP_PROP_POS_MSEC)))
-            frame_no += 1
-        else:
-            break
-
-    cap.release()
-
-def get_conversion_factor(vidpath, max_json_frame):
-    cap = cv.VideoCapture(vidpath)
-    max_vid_frame = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
-    print(f'[LOGGING: get_conversion_factor] max vid frame: {max_vid_frame} max json frame: {max_json_frame}')
-    return max_json_frame / max_vid_frame
 
 def get_data_for_frame(data, fnum, model_id):
     all_fnums = list(data.keys())
@@ -123,16 +81,6 @@ def get_data_for_frame(data, fnum, model_id):
 
     return poses[0].kps
 
-def get_data_for_adj_frame(data, fnum, model_id, cf):
-    fnum_adj = fnum * cf
-    # print(f'[LOGGING: get_data_for_adj_frame] original: {fnum} adjusted: {fnum_adj}')
-    return get_data_for_frame(data, fnum_adj, model_id)
-
-def get_frame_from_fnum(vidpath, fnum):
-    cap = cv.VideoCapture(vidpath)
-    cap.set(cv.CAP_PROP_POS_FRAMES, fnum-1)
-    _, frame = cap.read()
-    return frame
 
 def draw_pose_on_frame(frame, kps, kp_mapping=None, skeleton_list=None):
     for kp in kps.values():
@@ -152,51 +100,21 @@ def draw_pose_on_frame(frame, kps, kp_mapping=None, skeleton_list=None):
             
     return frame
 
-def frame_count(video_path, manual=True):
-    def manual_count(handler):
-        frames = 0
-        while True:
-            status, _ = handler.read()
-            if not status:
-                break
-            frames += 1
-        return frames 
-
-    cap = cv.VideoCapture(video_path)
-    if manual:
-        frames = manual_count(cap)
-    else:
-        try:
-            frames = int(cap.get(cv.CAP_PROP_FRAME_COUNT))
-        except:
-            frames = manual_count(cap)
-    cap.release()
-    return frames
-
-def get_length(filename):
-    result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
-                            "format=duration", "-of",
-                            "default=noprint_wrappers=1:nokey=1", filename],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT)
-    return float(result.stdout)
 
 if __name__ == '__main__': 
-    vid_path = '/Users/janyabudaraju/Desktop/curveassure/pose-sandbox/python-analysis/data/raw/test_video.MOV'
-    json_path = '/Users/janyabudaraju/Desktop/curveassure/pose-sandbox/python-analysis/data/raw/inference_data_2024-07-16T15-40-05-791Z.json'
+    vid_path = '/Users/janyabudaraju/Desktop/curveassure/pose-sandbox/python-analysis/data/raw/recorded_video_0701.mp4'
+    json_path = '/Users/janyabudaraju/Desktop/curveassure/pose-sandbox/python-analysis/data/raw/inference_data_2024-07-16T17-35-11-669Z.json'
 
     if vid_path.endswith('.webm'):
         new_path = Path(vid_path).with_suffix('.mp4')
-        convert_webm_to_mp4(vid_path, new_path)
+        vidutils.convert_webm_to_mp4(vid_path, new_path)
         vid_path = new_path
     
-    data, max_jf, max_jts, min_jts = clean_dict_from_JSON(json_path)
-    js_dur = max_jts - min_jts
-    vid_dur = get_length(vid_path)
-
-    print(f"json recorded duration: {js_dur} | video_duration: {vid_dur}")
-    # max_vf = frame_count(vid_path)
-    # print(f"max json frames: {max_jf} | max vid frames: {max_vf}")
+    data, max_jf, max_jts = clean_dict_from_JSON(json_path)
+    vid_dur = vidutils.get_length(vid_path)
+    print(f"json recorded duration: {max_jts} | video_duration: {vid_dur}")
+    max_vf = vidutils.frame_count(vid_path)
+    print(f"max json frames: {max_jf} | max vid frames: {max_vf}")
     #frame_cf = get_conversion_factor(vid_path, max_json_frame=max_jf)
     #print(frame_cf)
 
